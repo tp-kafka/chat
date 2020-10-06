8fd419d177b4d870ea930f02af26fbd79d920e8e
tobias.waller@googlemail.com <tobias.waller@googlemail.com>
2020-10-06 21:54:06 +0000

    wip

---
+++ src/test/java/tp/kafka/chat/ProduceExcercise.java
@@ -0,0 +1,50 @@
+package tp.kafka.chat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+
+@SpringBootTest
+@EmbeddedKafka
+class ProduceExcercise {
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafka;
+
+    Consumer<Object, Object> consumer;
+
+    @BeforeEach
+    public void setupConsumer(){
+        var consumerProps = KafkaTestUtils.consumerProps("ProduceExcercise", "true", this.embeddedKafka);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        var cf = new DefaultKafkaConsumerFactory<>(consumerProps);
+        this.consumer = cf.createConsumer();
+        this.embeddedKafka.consumeFromAnEmbeddedTopic(consumer, "chat");
+        this.embeddedKafka
+    }
+
+    @AfterEach
+    public void closeConsumer(){
+        this.consumer.close();
+    }
+
+	@Test
+	void succesfulMessage() throws Exception {
+        kafkaAdapter.sendChatMessage(new ChatMessage("userId", "message"));
+        
+        ConsumerRecords<Object, Object> messages = KafkaTestUtils.getRecords(consumer, 5000l);
+        assertThat(messages.count()).isGreaterThanOrEqualTo(1);
+	}
+
+}
