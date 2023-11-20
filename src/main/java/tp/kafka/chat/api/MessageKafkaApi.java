44b27a71bcaa6ab9b8ad623d030c72c7675b9cd4
i3oot <tobias.waller@googlemail.com>
2023-11-20 09:28:16 +0000

    receive message

---
+++ src/main/java/tp/kafka/chat/api/MessageKafkaApi.java
@@ -0,0 +1,23 @@
+package tp.kafka.chat.api;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.github.cjmatta.kafka.connect.irc.MessageEvent.Message;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class MessageKafkaApi {
+    
+    @KafkaListener(topics = "${topics.incoming.chat}")
+    public void listen(ConsumerRecord<String, Message> record) {
+        var channel = record.value().getChannel();
+        var nick = record.value().getSender().getNick();
+        var message = record.value().getMessage();
+        log.info("[{}] received message: ({}) {}", channel, nick, message);
+    }
+
+}
